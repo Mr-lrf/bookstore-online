a08263cb9fcce3a1eeb94a1d529ed144147bb3f8
liruifeng <695602338@qq.com>
2018-10-08 17:47:00 +0800

    update

---
+++ src/components/swiper.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="swiper-container">
+    <div class="swiper-wrapper">
+      <div class="swiper-slide"><img src="../../static/images/1.png" alt=""></div>
+      <div class="swiper-slide"><img src="../../static/images/2.png" alt=""></div>
+      <div class="swiper-slide"><img src="../../static/images/3.png" alt=""></div>
+    </div>
+    <!-- Add Pagination -->
+    <div class="swiper-pagination"></div>
+    <!-- Add Arrows -->
+    <!--<div class="swiper-button-next"></div>-->
+    <!--<div class="swiper-button-prev"></div>-->
+  </div>
+</template>
+
+<script>
+  import 'swiper/dist/css/swiper.css'
+  import Swiper from 'swiper'
+    export default {
+      methods: {
+        runSwiper() {
+          var swiper = new Swiper('.swiper-container', {
+            spaceBetween: 30,
+            centeredSlides: true,
+            autoplay: {
+              delay: 2500,
+              disableOnInteraction: false,
+            },
+            pagination: {
+              el: '.swiper-pagination',
+              clickable: true,
+            },
+            navigation: {
+              nextEl: '.swiper-button-next',
+              prevEl: '.swiper-button-prev',
+            },
+          });
+        }
+      },
+      mounted() {
+        this.runSwiper();
+      }
+    }
+</script>
+
+<style scoped>
+.swiper-slide img{
+  width: 100%;
+}
+
+</style>
--- src/pages/home/home.vue
+++ src/pages/home/home.vue
@@ -1,11 +1,18 @@
 <template>
-    <p>home</p>
+  <div>
+    <lrf-swiper></lrf-swiper>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "home"
+  import swiper from '@/components/swiper'
+  export default {
+    name: "home",
+    components:{
+      'lrf-swiper':swiper
     }
+
+  }
 </script>
 
 <style scoped>
