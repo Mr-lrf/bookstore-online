551e71ae2c160512fd96cc242c8c1088c3ac899b
liruifeng <695602338@qq.com>
2018-10-08 18:35:40 +0800

    update

--- src/components/swiper.vue
+++ src/components/swiper.vue
@@ -1,9 +1,7 @@
 <template>
   <div class="swiper-container">
     <div class="swiper-wrapper">
-      <div class="swiper-slide"><img src="../../static/images/1.png" alt=""></div>
-      <div class="swiper-slide"><img src="../../static/images/2.png" alt=""></div>
-      <div class="swiper-slide"><img src="../../static/images/3.png" alt=""></div>
+      <div class="swiper-slide" v-for="(item,index) in slides" :key="index"><img :src="item.src" alt=""></div>
     </div>
     <!-- Add Pagination -->
     <div class="swiper-pagination"></div>
@@ -16,36 +14,37 @@
 <script>
   import 'swiper/dist/css/swiper.css'
   import Swiper from 'swiper'
-    export default {
-      methods: {
-        runSwiper() {
-          var swiper = new Swiper('.swiper-container', {
-            spaceBetween: 30,
-            centeredSlides: true,
-            autoplay: {
-              delay: 2500,
-              disableOnInteraction: false,
-            },
-            pagination: {
-              el: '.swiper-pagination',
-              clickable: true,
-            },
-            navigation: {
-              nextEl: '.swiper-button-next',
-              prevEl: '.swiper-button-prev',
-            },
-          });
-        }
-      },
-      mounted() {
-        this.runSwiper();
+  export default {
+    props:['slides'],
+    methods: {
+      runSwiper() {
+        var swiper = new Swiper('.swiper-container', {
+          spaceBetween: 30,
+          centeredSlides: true,
+          autoplay: {
+            delay: 2500,
+            disableOnInteraction: false,
+          },
+          pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+          },
+          navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+          },
+        });
       }
+    },
+    mounted() {
+      this.runSwiper();
     }
+  }
 </script>
 
 <style scoped>
-.swiper-slide img{
-  width: 100%;
-}
+  .swiper-slide img{
+    width: 100%;
+  }
 
 </style>
--- src/pages/home/home.vue
+++ src/pages/home/home.vue
@@ -1,6 +1,10 @@
 <template>
   <div>
-    <lrf-swiper></lrf-swiper>
+    <lrf-swiper :slides="imgDatas"></lrf-swiper>
+    <div class="ads">
+      <i class="iconfont icon-zhong"></i>
+      <marquee>双12全场图书低至1折，更有隐藏神秘惊喜</marquee>
+    </div>
   </div>
 </template>
 
@@ -10,11 +14,31 @@
     name: "home",
     components:{
       'lrf-swiper':swiper
+    },
+    data(){
+      return{
+        imgDatas:[{
+          src:'../../static/images/1.png'
+        },{
+          src:'../../static/images/2.png'
+        },{
+          src:'../../static/images/3.png'
+        }]
+      }
     }
 
   }
 </script>
 
 <style scoped>
-
+  .ads{
+    padding: 0 0.2rem;
+  }
+  .ads marquee{
+    width: 80vw;
+    font-size: 0.02rem;
+  }
+  .icon-zhong{
+    color: orange;
+  }
 </style>
